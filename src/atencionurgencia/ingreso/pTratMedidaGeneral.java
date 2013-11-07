86cfb499138a47b22aa6891a422450c7143ad9a6
Administrador <Administrador@Desarrollador1>
2013-11-07 10:40:47 -0500

    add + datos evolucion

--- src/atencionurgencia/ingreso/pTratMedidaGeneral.java
+++ src/atencionurgencia/ingreso/pTratMedidaGeneral.java
@@ -5,6 +5,8 @@
 package atencionurgencia.ingreso;
 
 import atencionurgencia.AtencionUrgencia;
+import entidades.HcuEvoMedidasg;
+import entidades.HcuEvolucion;
 import entidades.InfoHistoriac;
 import entidades.InfoMedidasgHcu;
 import java.util.List;
@@ -13,6 +15,8 @@ import javax.swing.JOptionPane;
 import javax.swing.ListSelectionModel;
 import javax.swing.SwingUtilities;
 import javax.swing.table.DefaultTableModel;
+import jpa.HcuEvoMedidasgJpaController;
+import jpa.HcuEvolucionJpaController;
 import jpa.InfoMedidasgHcuJpaController;
 import jpa.exceptions.NonexistentEntityException;
 import tools.Funciones;
@@ -23,14 +27,27 @@ import tools.Funciones;
  */
 public class pTratMedidaGeneral extends javax.swing.JPanel {
     private DefaultTableModel ModeloTabla;
-    private Object dato[] = null;
+    private final Object dato[] = null;
     private List<InfoMedidasgHcu> listMedidasG;
+    private List<HcuEvoMedidasg> evoMedidasgs;
     private InfoMedidasgHcuJpaController medidasgHcuJPA=null;
+    private HcuEvoMedidasgJpaController hcuEvoMedidasgJpa=null;
+    private boolean evo=false;
 
     /**
      * Creates new form pTratMedidaGeneral
      */
     public pTratMedidaGeneral() {
+        initComponents();
+        setCargaTabla();
+    }
+    
+    /**
+     * 
+     * @param evo true para evolucion
+     */
+    public pTratMedidaGeneral(boolean evo){
+        this.evo=evo;
         initComponents();
         setCargaTabla();
     }
@@ -131,6 +148,61 @@ public class pTratMedidaGeneral extends javax.swing.JPanel {
         }
     }
     
+    public void saveChanges(EntityManagerFactory factory, HcuEvolucion evol){
+        if(hcuEvoMedidasgJpa==null){
+            hcuEvoMedidasgJpa=new HcuEvoMedidasgJpaController(factory);
+        }
+        evoMedidasgs = hcuEvoMedidasgJpa.ListFindHcuEvoMedidasG(evol);
+        for (int i = 0; i < ModeloTabla.getRowCount(); i++) {
+            HcuEvoMedidasg hem=null;
+            boolean existe=false;
+            for(HcuEvoMedidasg hem1:evoMedidasgs){
+                if(((String)ModeloTabla.getValueAt(i, 0)).equals(hem1.getMedidag())){
+                    existe=true;
+                    hem=hem1;
+                    break;
+                }
+            }
+            if(!existe){
+                hem=new HcuEvoMedidasg();
+                hem.setIdHcuEvolucion(evol);
+                hem.setMedidag((String)ModeloTabla.getValueAt(i, 0));
+                hem.setObservacion((String)ModeloTabla.getValueAt(i, 1));
+                hem.setIdUsuario(AtencionUrgencia.configdecripcionlogin.getId());
+                hem.setEstado(1);//1 es activo
+                hcuEvoMedidasgJpa.create(hem);
+            }else{
+                if(hem!=null){
+                    try {
+                        hem.setMedidag((String)ModeloTabla.getValueAt(i, 0));
+                        hem.setObservacion((String)ModeloTabla.getValueAt(i, 1));
+                        hem.setIdUsuario(AtencionUrgencia.configdecripcionlogin.getId());
+                        hcuEvoMedidasgJpa.edit(hem);
+                    } catch (Exception ex) {
+                        JOptionPane.showMessageDialog(null, "10117:\n"+ex.getMessage(), pTratMedidaGeneral.class.getName(), JOptionPane.INFORMATION_MESSAGE);
+                    }
+                }
+            }
+        }
+        for(HcuEvoMedidasg hem:evoMedidasgs){
+            boolean existe=false;
+            for (int i = 0; i < ModeloTabla.getRowCount(); i++) {
+                if(((String)ModeloTabla.getValueAt(i, 0)).equals(hem.getMedidag())){
+                    existe=true;
+                    break;
+                }
+            }
+            if(!existe){
+                try {
+                    hem.setEstado(0);//inactivo
+                    hcuEvoMedidasgJpa.edit(hem);
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(null, "10108:\n"+ex.getMessage(), pTratMedidaGeneral.class.getName(), JOptionPane.INFORMATION_MESSAGE);
+                }
+            }
+        }
+    }
+    
     public void showListExistentes(EntityManagerFactory factory,InfoHistoriac ihc){
         if(medidasgHcuJPA==null){
             medidasgHcuJPA=new InfoMedidasgHcuJpaController(factory);
@@ -141,6 +213,80 @@ public class pTratMedidaGeneral extends javax.swing.JPanel {
             ModeloTabla.setValueAt(listMedidasG.get(i).getMedidag(), i, 0);
             ModeloTabla.setValueAt(listMedidasG.get(i).getObservacion(), i, 1);
         }
+    }
+    
+    public void showListExistentes(EntityManagerFactory factory,HcuEvolucion evol){
+        if(hcuEvoMedidasgJpa==null){
+            hcuEvoMedidasgJpa=new HcuEvoMedidasgJpaController(factory);
+        }
+        evoMedidasgs=hcuEvoMedidasgJpa.ListFindHcuEvoMedidasG(evol);
+        for(int i=0;i<evoMedidasgs.size();i++){
+            ModeloTabla.addRow(dato);
+            ModeloTabla.setValueAt(evoMedidasgs.get(i).getMedidag(), i, 0);
+            ModeloTabla.setValueAt(evoMedidasgs.get(i).getObservacion(), i, 1);
+        }
+        /* Comprobamos que la evolucion no contiene medidas generales */
+        if(evoMedidasgs.isEmpty()){
+            List<HcuEvoMedidasg> medidasgs=null;
+            HcuEvolucionJpaController hejc = new HcuEvolucionJpaController(factory);
+            /* consulta de las evoluciones que pertenecen a la nota de ingreso */
+            List<HcuEvolucion> hes= hejc.FindHcuEvolucions(evol.getIdInfoHistoriac());            
+            /*
+             * verifica si tiene evoluciones y toma la ultima listada.
+             * el orden de la fecha es ascendente
+             */
+            if(!hes.isEmpty()){
+                medidasgs = hes.get(hes.size()-1).getHcuEvoMedidasgs();
+                /* verificamos si la ultima evolucion tiene medidas generales. 
+                 *   --> si tiene medidasG en la ultima evolucion hacemos la migracion
+                 *   --> a la evolucion que se esta creando
+                 */
+                if(!medidasgs.isEmpty() && evol.getFechaEvo().compareTo(hes.get(hes.size()-1).getFechaEvo())>0 ){
+                    /* preguntamos si se desea hacer la migracion */
+                    this.migrarMediGeneToEvo(medidasgs, true);
+                    
+                }
+            }else{
+                /* Si no tiene evoluciones buscamos las medidas de la hcu */
+                InfoMedidasgHcuJpaController imhjc = new InfoMedidasgHcuJpaController(factory);
+                List<InfoMedidasgHcu> imh = imhjc.ListFindInfoMedidasGHcu(evol.getIdInfoHistoriac());
+                /* verificamos que la hcu tiene medidasG */
+                if(!imh.isEmpty()){
+                    this.migrarMediGeneToEvo(medidasgs, false);
+                }
+            }
+        }
+    }
+    
+    /**
+     * 
+     * @param medidas List de medidaGhcu or medidaEvo
+     * @param evo true si viene de evolucion; false si viene de hcu
+     */
+    private void migrarMediGeneToEvo(Object medidas, boolean evo){
+        String mensaje = "¿Quiere continuar con las medidas generales de la Nota de Ingreso? ";
+        if(evo){
+            mensaje = "¿Quiere continuar con las medidas generales de la Evolución anterior? ";
+        }
+        int entrada = JOptionPane.showConfirmDialog(null, mensaje,"Migración de medidas generales",JOptionPane.YES_NO_OPTION);
+        if(entrada==0){
+            if(evo){
+                List<HcuEvoMedidasg> medidasgs = (List<HcuEvoMedidasg>) medidas;
+                for(int i=0;i<medidasgs.size();i++){
+                    ModeloTabla.addRow(dato);
+                    ModeloTabla.setValueAt(medidasgs.get(i).getMedidag(), i, 0);
+                    ModeloTabla.setValueAt(medidasgs.get(i).getObservacion(), i, 1);
+                }
+            }else{
+                List<InfoMedidasgHcu> imh = (List<InfoMedidasgHcu>) medidas;
+                for(int i=0;i<imh.size();i++){
+                    ModeloTabla.addRow(dato);
+                    ModeloTabla.setValueAt(imh.get(i).getMedidag(), i, 0);
+                    ModeloTabla.setValueAt(imh.get(i).getObservacion(), i, 1);
+                }
+            }
+        }
+
     }
 
     /**
@@ -153,8 +299,6 @@ public class pTratMedidaGeneral extends javax.swing.JPanel {
     private void initComponents() {
 
         jLabel49 = new javax.swing.JLabel();
-        buttonSeven6 = new org.edisoncor.gui.button.ButtonSeven();
-        buttonSeven7 = new org.edisoncor.gui.button.ButtonSeven();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
         jPanel32 = new javax.swing.JPanel();
@@ -162,6 +306,8 @@ public class pTratMedidaGeneral extends javax.swing.JPanel {
         jTextArea26 = new javax.swing.JTextArea();
         jScrollPane27 = new javax.swing.JScrollPane();
         jTextArea27 = new javax.swing.JTextArea();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
 
         setFocusable(false);
         setMaximumSize(new java.awt.Dimension(380, 420));
@@ -172,28 +318,6 @@ public class pTratMedidaGeneral extends javax.swing.JPanel {
         jLabel49.setFont(new java.awt.Font("Tahoma", 1, 10)); // NOI18N
         jLabel49.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/Edit.png"))); // NOI18N
         jLabel49.setText("MEDIDAS GENERALES");
-
-        buttonSeven6.setForeground(new java.awt.Color(0, 0, 255));
-        buttonSeven6.setText("Agregar");
-        buttonSeven6.setColorBrillo(new java.awt.Color(255, 255, 255));
-        buttonSeven6.setColorDeSombra(new java.awt.Color(255, 255, 255));
-        buttonSeven6.setFont(new java.awt.Font("Arial", 0, 10)); // NOI18N
-        buttonSeven6.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                buttonSeven6ActionPerformed(evt);
-            }
-        });
-
-        buttonSeven7.setForeground(new java.awt.Color(0, 0, 255));
-        buttonSeven7.setText("Borrar");
-        buttonSeven7.setColorBrillo(new java.awt.Color(255, 255, 255));
-        buttonSeven7.setColorDeSombra(new java.awt.Color(255, 255, 255));
-        buttonSeven7.setFont(new java.awt.Font("Arial", 0, 10)); // NOI18N
-        buttonSeven7.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                buttonSeven7ActionPerformed(evt);
-            }
-        });
 
         jScrollPane1.setBackground(new java.awt.Color(255, 255, 255));
         jScrollPane1.setOpaque(false);
@@ -226,6 +350,7 @@ public class pTratMedidaGeneral extends javax.swing.JPanel {
         jScrollPane26.setMinimumSize(new java.awt.Dimension(164, 20));
         jScrollPane26.setPreferredSize(new java.awt.Dimension(164, 20));
 
+        jTextArea26.setEditable(false);
         jTextArea26.setColumns(20);
         jTextArea26.setFont(new java.awt.Font("Tahoma", 0, 10)); // NOI18N
         jTextArea26.setForeground(new java.awt.Color(0, 102, 255));
@@ -264,6 +389,22 @@ public class pTratMedidaGeneral extends javax.swing.JPanel {
         jTextArea27.setMinimumSize(new java.awt.Dimension(164, 40));
         jScrollPane27.setViewportView(jTextArea27);
 
+        jButton1.setText("Agregar");
+        jButton1.setFocusable(false);
+        jButton1.setOpaque(false);
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jButton2.setText("Borrar");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
@@ -276,9 +417,9 @@ public class pTratMedidaGeneral extends javax.swing.JPanel {
                     .addComponent(jPanel32, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jScrollPane27, javax.swing.GroupLayout.DEFAULT_SIZE, 360, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(buttonSeven6, javax.swing.GroupLayout.PREFERRED_SIZE, 92, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jButton1)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(buttonSeven7, javax.swing.GroupLayout.PREFERRED_SIZE, 92, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 76, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -287,32 +428,22 @@ public class pTratMedidaGeneral extends javax.swing.JPanel {
                 .addComponent(jLabel49)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jScrollPane27, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(10, 10, 10)
+                .addGap(9, 9, 9)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(buttonSeven6, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(buttonSeven7, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jButton1)
+                    .addComponent(jButton2))
                 .addGap(18, 18, 18)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 182, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 180, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jPanel32, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
 
-    private void buttonSeven6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonSeven6ActionPerformed
-        this.cargaDato();
-    }//GEN-LAST:event_buttonSeven6ActionPerformed
-
-    private void buttonSeven7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonSeven7ActionPerformed
-        if(ModeloTabla.getRowCount()>0 && jTable1.getSelectedRow()>-1){
-            ModeloTabla.removeRow(jTable1.getSelectedRow());
-            jTextArea26.setText("");
-        }
-    }//GEN-LAST:event_buttonSeven7ActionPerformed
-
     private void jTable1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTable1MouseClicked
         if(SwingUtilities.isLeftMouseButton(evt) && evt.getClickCount()==1){
             jTextArea26.setText((String)jTable1.getValueAt(jTable1.getSelectedRow(), 1));
+            jTextArea26.setEditable(true);
         }
     }//GEN-LAST:event_jTable1MouseClicked
 
@@ -330,9 +461,20 @@ public class pTratMedidaGeneral extends javax.swing.JPanel {
         }
     }//GEN-LAST:event_jTextArea26KeyReleased
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        this.cargaDato();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        if(ModeloTabla.getRowCount()>0 && jTable1.getSelectedRow()>-1){
+            ModeloTabla.removeRow(jTable1.getSelectedRow());
+            jTextArea26.setText("");
+        }
+    }//GEN-LAST:event_jButton2ActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    public org.edisoncor.gui.button.ButtonSeven buttonSeven6;
-    public org.edisoncor.gui.button.ButtonSeven buttonSeven7;
+    public javax.swing.JButton jButton1;
+    public javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel49;
     private javax.swing.JPanel jPanel32;
     private javax.swing.JScrollPane jScrollPane1;
