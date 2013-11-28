c9ca5b7fe6676066cab75e416ee8a3f138de4afa
Alvaro Monsalve <alvaromonsalve@yahoo.com.co>
2013-11-28 14:25:40 -0500

    anexo de validacion para acceso

--- src/atencionurgencia/AtencionUrgencia.java
+++ src/atencionurgencia/AtencionUrgencia.java
@@ -4,11 +4,18 @@
  */
 package atencionurgencia;
 
+import entidades.AccessConfigUser;
+import entidades.AccessRoles;
 import entidades.Configdecripcionlogin;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.util.List;
 import java.util.Properties;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
+import jpa.AccessConfigUserJpaController;
 import jpa.ConfigdecripcionloginJpaController;
 
 
@@ -30,11 +37,41 @@ public class AtencionUrgencia {
      * @return panel principal de la clase setBounds(0, 0, 840, 540)
      */
     public static JPanel getPanelIndex(int idUsuario2, Properties props){
+        JPanel jPanel = new Panel("/images/permiso.png");
         EntityManagerFactory factory=Persistence.createEntityManagerFactory("ClipaEJBPU",props);
         ConfigdecripcionloginJpaController configdecripcionloginJpaController = new ConfigdecripcionloginJpaController(factory);
         configdecripcionlogin = configdecripcionloginJpaController.findConfigdecripcionlogin(idUsuario2);
-        panelindex = new panelIndex();
-        AtencionUrgencia.props =props;
-        return panelindex;
+        AccessConfigUserJpaController acujc = new AccessConfigUserJpaController(factory);
+        List<AccessConfigUser> ACU = acujc.FindConfigUsers(configdecripcionlogin);
+        for(AccessConfigUser accessConfigUser:ACU){
+            List<AccessRoles> roles = accessConfigUser.getIdPerfiles().getAccessRolesList();
+            for(AccessRoles ar:roles){
+                if(ar.getRuta()==10000){
+                    panelindex = new panelIndex();
+                    AtencionUrgencia.props =props;       
+                    jPanel = panelindex;
+                }
+            }
+        }
+        return jPanel;
+    }
+    
+    private static class Panel extends JPanel{
+        ImageIcon imagen;
+        String nombre;
+        
+        public Panel(String nombre){
+            this.nombre = nombre;
+        }
+        
+        @Override
+        public void paint(Graphics g){
+            Dimension tamanio = getSize();
+            imagen=new ImageIcon(getClass().getResource(nombre));
+            g.drawImage(imagen.getImage(), 0, 0, tamanio.width,tamanio.height,null);
+            setOpaque(false);
+            super.paint(g);
+        }
+        
     }
 }
